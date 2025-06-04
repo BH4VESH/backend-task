e37fc37178f4be67067264b4ec206a0e90bfe09d
BH4VESH <narolabhavesh480@gmail.com>
2025-06-05 00:08:12 +0530

    remove unnesasory code

--- dist/app.js
+++ dist/app.js
@@ -11,11 +11,4 @@ app.use(express_1.default.json());
 app.use('/api/part', part_routes_1.default);
 app.use(error_middleware_1.errorHandler);
 exports.default = app;
-// import express from 'express';
-// import partRoutes from './routes/part.routes';
-// import { errorHandler } from './middleware/error.middleware';
-// const app = express();
-// app.use(express.json());
-// app.use('/api', partRoutes);
-// app.use(errorHandler);
-// export default app;
+
