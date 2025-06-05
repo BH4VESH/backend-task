bdb29ddf786e9216d31d300b8c10cd6900a38568
bhavesh <bnarola@elluminatinc.com>
2025-06-05 11:05:53 +0530

    remove commented code

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,3 @@ app.use(errorHandler);
 export default app;
 
 
-
-// import express from 'express';
-// import partRoutes from './routes/part.routes';
-// import { errorHandler } from './middleware/error.middleware';
-
-// const app = express();
-
-// app.use(express.json());
-// app.use('/api', partRoutes);
-// app.use(errorHandler);
-
-// export default app;
